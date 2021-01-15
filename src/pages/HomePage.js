d37e7765493eec6e9a6cac55c9bd3c010fceac82
鞠云鹏 <76542009@qq.com>
2021-01-15 23:22:53 +0800

    add

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,70 +1,93 @@
-import React,{Component} from 'react'
-import {View,Text,ImageBackground,StyleSheet,SafeAreaView,Image,TextInput} from 'react-native'
+import React, {Component} from 'react';
+
+import {
+    View,
+    Text,
+    ImageBackground,
+    StyleSheet,
+    SafeAreaView,
+    Image,
+    TextInput,
+} from 'react-native';
+
 import Icon from '../assets/iconfont/';
-class HomePage extends  Component {
+import HomeSwiper from '../component/swiper';
+class HomePage extends Component {
     constructor() {
         super();
     }
-    render(){
-        return(
+
+    render() {
+        return (
             <View>
-               <View>
-                   <ImageBackground source={require('../assets/home/headerbg.png')}
-                                    style={styles.headerbg}  >
-                    <SafeAreaView>
-                     <View style={styles.headerContainer}>
-                         <View>
-                             <Text style={styles.headerText}>全部</Text>
-                         </View>
-                         <View>
-                            <Image source={require('../assets/home/headermenu.png')} style={styles.headerMenu}/>
-                         </View>
-                     </View>
-                        <View style={{marginTop:20,marginLeft: 15,marginRight: 15,
-                            shadowColor:"#333333",elevation:3,
-                            shadowOffset: { width: 0, height: 0 },  // 阴影偏移
-                            shadowOpacity: 0.2,  // 阴影不透明度
-                            shadowRadius: 10, }}>
-                            <TextInput   placeholder={'请输入款式号搜索'}
-                                         placeholderStyle={{paddingLeft:10}}
-                                         placeholderTextColor={"#999999"}
-                                style={{
-                                    height:44,
-                                    backgroundColor:"#ffffff",
-                                     }}/>
-                        </View>
-
-                    </SafeAreaView>
-                   </ImageBackground>
-
-               </View>
                 <View>
-                    <Text>测试</Text>
-                    <Icon name="iconcainixihuan" />
+                    <ImageBackground
+                        source={require('../assets/home/headerbg.png')}
+                        style={styles.headerbg}>
+                        <SafeAreaView>
+                            <View style={styles.headerContainer}>
+                                <View>
+                                    <Text style={styles.headerText}>全部</Text>
+                                </View>
+                                <View>
+                                    <Image
+                                        source={require('../assets/home/headermenu.png')}
+                                        style={styles.headerMenu}
+                                    />
+                                </View>
+                            </View>
+                            <View
+                                style={{
+                                    marginTop: 20,
+                                    marginLeft: 15,
+                                    marginRight: 15,
+                                    shadowColor: '#333333',
+                                    elevation: 3,
+                                    shadowOffset: {width: 0, height: 0}, // 阴影偏移
+                                    shadowOpacity: 0.2, // 阴影不透明度
+                                    shadowRadius: 10,
+                                }}>
+                                <TextInput
+                                    placeholder={'请输入款式号搜索'}
+                                    placeholderStyle={{paddingLeft: 10}}
+                                    placeholderTextColor={'#999999'}
+                                    style={{
+                                        height: 44,
+                                        backgroundColor: '#ffffff',
+                                    }}
+                                />
+                            </View>
+                        </SafeAreaView>
+                    </ImageBackground>
+                </View>
+                <View style={{height:160}}>
+                    <HomeSwiper/>
                 </View>
             </View>
-        )
+        );
+        ;
     }
 }
-export  default  HomePage
+
+export default HomePage;
 const styles = StyleSheet.create({
-    headerbg:{
-        width:'100%',
-        height:246
+    headerbg: {
+        width: '100%',
+        height: 146,
     },
-    headerContainer:{
-        display:"flex",
-        justifyContent:"space-between",
-        flexDirection:"row",
-        marginLeft:15,
-        marginRight:15
+    headerContainer: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        marginLeft: 15,
+        marginRight: 15,
     },
-    headerText:{
-        fontSize:18,
-        fontWeight:'500'
+    headerText: {
+        fontSize: 18,
+        fontWeight: '500',
     },
-    headerMenu:{
-        width:22,
-        height: 22
-    }
-})
+    headerMenu: {
+        width: 22,
+        height: 22,
+    },
+});
--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,12 +1,42 @@
-import React,{Component} from 'react'
-import {View,Text} from 'react-native'
-class MyPage extends  Component {
-    render(){
-        return(
-            <View>
-                <Text>我的</Text>
-            </View>
-        )
+import React from 'react'
+import {Image, Text, View} from 'react-native';
+import Swiper from 'react-native-swiper'
+
+var styles = {
+    wrapper: {},
+    slide1: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#9DD6EB'
+    },
+    slide2: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#97CAE5'
+    },
+    slide3: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#92BBD9'
+    },
+    text: {
+        color: '#fff',
+        fontSize: 30,
+        fontWeight: 'bold'
     }
 }
-export  default  MyPage
+
+export default () => (
+    <Swiper style={styles.wrapper} showsButtons loop={false}>
+        <View testID="Hello" style={styles.slide1}>
+            <Image source={require('../assets/home/banner1.png')} style={{width:'100%',height:330}} />
+        </View>
+        <View testID="Beautiful" style={styles.slide2}>
+            <Image source={require('../assets/home/banner2.png')} style={{width:'100%',height:330}} />
+        </View>
+
+    </Swiper>
+)
